ed7a78d7bf59fe4db6f436b7e194dc3ce2b67f5f
Magnus <mp-se@noreply.github.com>
2025-07-08 23:22:02 +0200

    Added option to filter data based on device id

--- src/views/MeasurementView.vue
+++ src/views/MeasurementView.vue
@@ -21,6 +21,16 @@
       <div class="col-md-12">&nbsp;</div>
 
       <template v-if="deviceType === 0">
+
+        <div class="col-md-12">
+          <BsSelect
+            v-model="gravitymonDevice"
+            :options="gravitymonDeviceOptions"
+            label="Device"
+            width="5"
+          ></BsSelect>
+        </div>
+
         <div class="table-responsive">
           <table class="table table-sm table-bordered">
             <thead class="table-primary">
@@ -40,7 +50,7 @@
               </tr>
             </thead>
             <tbody>
-              <tr v-for="(entry, idx) in measurement.gravitymonData" :key="idx">
+              <tr v-for="(entry, idx) in filteredGravitymonData" :key="idx">
                 <td>{{ idx + 1 }}</td>
                 <td>
                   {{
@@ -65,6 +75,16 @@
         </div>
       </template>
       <template v-if="deviceType === 1">
+
+        <div class="col-md-12">
+          <BsSelect
+            v-model="tiltDevice"
+            :options="tiltDeviceOptions"
+            label="Device"
+            width="5"
+          ></BsSelect>
+        </div>
+
         <div class="table-responsive">
           <table class="table table-sm table-bordered">
             <thead class="table-primary">
@@ -81,7 +101,7 @@
               </tr>
             </thead>
             <tbody>
-              <tr v-for="(entry, idx) in measurement.tiltData" :key="idx">
+              <tr v-for="(entry, idx) in filteredTiltData" :key="idx">
                 <td>{{ idx + 1 }}</td>
                 <td>
                   {{
@@ -103,6 +123,16 @@
         </div>
       </template>
       <template v-if="deviceType === 2">
+
+        <div class="col-md-12">
+          <BsSelect
+            v-model="pressuremonDevice"
+            :options="pressuremonDeviceOptions"
+            label="Device"
+            width="5"
+          ></BsSelect>
+        </div>
+
         <div class="table-responsive">
           <table class="table table-sm table-bordered">
             <thead class="table-primary">
@@ -122,7 +152,7 @@
               </tr>
             </thead>
             <tbody>
-              <tr v-for="(entry, idx) in measurement.pressuremonData" :key="idx">
+              <tr v-for="(entry, idx) in filteredPressuremonData" :key="idx">
                 <td>{{ idx + 1 }}</td>
                 <td>
                   {{
@@ -147,6 +177,16 @@
         </div>
       </template>
       <template v-if="deviceType === 3">
+
+                <div class="col-md-12">
+          <BsSelect
+            v-model="chamberControllerDevice"
+            :options="chamberControllerDeviceOptions"
+            label="Device"
+            width="5"
+          ></BsSelect>
+        </div>
+
         <div class="table-responsive">
           <table class="table table-sm table-bordered">
             <thead class="table-primary">
@@ -184,13 +224,27 @@
 
 <script setup>
 import { measurement, global, status } from '@/modules/pinia'
-import { ref, watch, onBeforeMount, onBeforeUnmount } from 'vue'
+import { ref, watch, onBeforeMount, onBeforeUnmount, computed } from 'vue'
 import { logDebug } from '@/modules/logger'
+import BsDropdown from '@/components/BsDropdown.vue'
+import BsSelect from '@/components/BsSelect.vue'
 
 const deviceType = ref(-1)
 const deviceTypeOptions = ref([
   //  { label: 'Text', value: 0 },
 ])
+
+
+const gravitymonDevice = ref('')
+const tiltDevice = ref('')
+const pressuremonDevice = ref('')
+const chamberControllerDevice = ref('')
+
+const gravitymonDeviceOptions = ref([])
+const tiltDeviceOptions = ref([])
+const pressuremonDeviceOptions = ref([])
+const chamberControllerDeviceOptions = ref([])
+
 
 onBeforeMount(() => {
   measurement.updateMeasurementFiles((success) => {
@@ -202,19 +256,72 @@ onBeforeMount(() => {
 
         if (measurement.gravitymonData.length > 0) {
           deviceTypeOptions.value.push({ label: 'Gravitymon', value: 0 })
+
+          const seenIds = new Set()
+          gravitymonDeviceOptions.value = measurement.gravitymonData
+            .filter((entry) => {
+              if (seenIds.has(entry.getId())) return false
+              seenIds.add(entry.getId())
+              return true
+            })
+            .map((entry) => ({
+              label: entry.getId() + ' - ' + entry.getName(),
+              value: entry.getId()
+            }))
+          gravitymonDeviceOptions.value.push({ label: 'All devices', value: '' })
         }
 
         if (measurement.tiltData.length > 0) {
           deviceTypeOptions.value.push({ label: 'Tilt', value: 1 })
+
+          const seenTiltIds = new Set()
+          tiltDeviceOptions.value = measurement.tiltData
+            .filter((entry) => {
+              if (seenTiltIds.has(entry.getId())) return false
+              seenTiltIds.add(entry.getId())
+              return true
+            })
+            .map((entry) => ({
+              label: entry.getId() + ' - ' + entry.getColor(),
+              value: entry.getId()
+            }))
+          tiltDeviceOptions.value.push({ label: 'All devices', value: '' })
         }
 
         if (measurement.pressuremonData.length > 0) {
           deviceTypeOptions.value.push({ label: 'Pressuremon', value: 2 })
+
+          const seenPressureIds = new Set()
+          pressuremonDeviceOptions.value = measurement.pressuremonData
+            .filter((entry) => {
+              if (seenPressureIds.has(entry.getId())) return false
+              seenPressureIds.add(entry.getId())
+              return true
+            })
+            .map((entry) => ({
+              label: entry.getId() + ' - ' + entry.getName(),
+              value: entry.getId()
+            }))
+          pressuremonDeviceOptions.value.push({ label: 'All devices', value: '' })
         }
 
         if (measurement.chamberControllerData.length > 0) {
           deviceTypeOptions.value.push({ label: 'Chamber Controller', value: 3 })
+
+          const seenChamberIds = new Set()
+          chamberControllerDeviceOptions.value = measurement.chamberControllerData
+            .filter((entry) => {
+              if (seenChamberIds.has(entry.getId())) return false
+              seenChamberIds.add(entry.getId())
+              return true
+            })
+            .map((entry) => ({
+              label: entry.getId() + ' - ' + entry.getName(),
+              value: entry.getId()
+            }))
+          chamberControllerDeviceOptions.value.push({ label: 'All devices', value: '' })
         }
+
       })
     } else {
       global.errorMessage = 'Failed to fetch list of measurement files'
@@ -229,5 +336,18 @@ watch(deviceType, (newValue, oldValue) => {
   logDebug('MeasurementView.watch()', 'Selected deviceType:', newValue)
 
 
+})
+
+const filteredGravitymonData = computed(() => {
+  if (!gravitymonDevice.value) return measurement.gravitymonData
+  return measurement.gravitymonData.filter(entry => entry.getId() === gravitymonDevice.value)
+})
+const filteredTiltData = computed(() => {
+  if (!tiltDevice.value) return measurement.tiltData
+  return measurement.tiltData.filter(entry => entry.getId() === tiltDevice.value)
+})
+const filteredPressuremonData = computed(() => {
+  if (!pressuremonDevice.value) return measurement.pressuremonData
+  return measurement.pressuremonData.filter(entry => entry.getId() === pressuremonDevice.value)
 })
 </script>
