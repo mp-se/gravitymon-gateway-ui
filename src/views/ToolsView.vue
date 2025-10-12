730e43ae5bfe634caff8d1416ea29d201ca7817a
Magnus <mp-se@noreply.github.com>
2025-10-12 15:02:52 +0200

    Update standard views from gravitymon

--- src/views/ToolsView.vue
+++ src/views/ToolsView.vue
@@ -2,44 +2,23 @@
   <div class="container">
     <p></p>
     <p class="h3">Tools</p>
-
-    <div class="col-md-12" v-if="status.sd_mounted">
-      <BsInputRadio
-        v-model="fileSystem"
-        :options="fileSystemOptions"
-        label="Select File System"
-        width=""
-      ></BsInputRadio>
-    </div>
     <hr />
 
-    <template v-if="fileSystem == 0">
-      <ListFilesFragment></ListFilesFragment>
-      <div class="row gy-4">
-        &nbsp;
-        <hr />
-      </div>
-      <AdvancedFilesFragment v-if="!hideAdvanced"></AdvancedFilesFragment>
+    <template v-if="global.ui.enableVoltageFragment">
+      <VoltageFragment></VoltageFragment>
 
-      <div class="row gy-4" v-if="!hideAdvanced">
-        &nbsp;
+      <div class="row gy-4">
+        <p></p>
         <hr />
       </div>
     </template>
 
-    <template v-else>
-      <ListSecureCardFragment></ListSecureCardFragment>
-      <div class="row gy-4">
-        &nbsp;
-        <hr />
-      </div>
-      <AdvancedSecureDiskFragment v-if="!hideAdvanced"></AdvancedSecureDiskFragment>
+    <ListFilesFragment></ListFilesFragment>
 
-      <div class="row gy-4" v-if="!hideAdvanced">
-        &nbsp;
-        <hr />
-      </div>
-    </template>
+    <div class="row gy-4">
+      <p></p>
+      <hr />
+    </div>
 
     <div class="row gy-4" v-if="hideAdvanced">
       <div class="col-md-2">
@@ -53,11 +32,18 @@
             class="spinner-border spinner-border-sm"
             role="status"
             aria-hidden="true"
-            :hidden="!global.disabled"
+            v-show="global.disabled"
           ></span>
           &nbsp;Enable Advanced
         </button>
       </div>
+    </div>
+
+    <AdvancedFilesFragment v-if="!hideAdvanced"></AdvancedFilesFragment>
+
+    <div class="row gy-4" v-if="!hideAdvanced">
+      <p></p>
+      <hr />
     </div>
 
     <EnableCorsFragment v-if="!hideAdvanced"></EnableCorsFragment>
@@ -66,20 +52,9 @@
 
 <script setup>
 import { ref } from 'vue'
-import { global, status } from '@/modules/pinia'
-import ListFilesFragment from '@/fragments/ListFilesFragment.vue'
-import AdvancedFilesFragment from '@/fragments/AdvancedFilesFragment.vue'
-import AdvancedSecureDiskFragment from '@/fragments/AdvancedSecureDiskFragment.vue'
-import ListSecureCardFragment from '@/fragments/ListSecureDiskFragment.vue'
-import EnableCorsFragment from '@/fragments/EnableCorsFragment.vue'
+import { global } from '@/modules/pinia'
 
 const hideAdvanced = ref(true)
-const fileSystem = ref(0)
-
-const fileSystemOptions = ref([
-  { label: 'Internal Flash', value: 0 },
-  { label: 'Secure Digital (SD)', value: 1 }
-])
 
 function enableAdvanced() {
   hideAdvanced.value = !hideAdvanced.value
